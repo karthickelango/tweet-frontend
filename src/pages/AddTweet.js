977f288263cd809e76540c3c8ee132cd080cadeb
karthickelango <karthik5ive@gmail.com>
2024-02-07 15:31:59 +0530

    add uri

--- src/pages/AddTweet.js
+++ src/pages/AddTweet.js
@@ -7,7 +7,7 @@ import DataContext from '../context/DataContext'
 import { BASE_URL, TWEET_URI } from '../constants/api_urls'
 
 const AddTweet = () => {
-    const {activeUser} = useContext(DataContext)
+    const { activeUser } = useContext(DataContext)
     const [tweet, setTweet] = useState('')
     const [isLoading, setIsLoading] = useState(false)
     const navigate = useNavigate()
@@ -22,7 +22,7 @@ const AddTweet = () => {
     // get active user
     const getUser = async () => {
         try {
-            const response = await axios.get(`${BASE_URL}/${activeUser}`) 
+            const response = await axios.get(`${BASE_URL}/${activeUser}`)
             if (response.status >= 200 && response.status <= 299) {
                 setUserActive(response.data)
             }
@@ -47,13 +47,16 @@ const AddTweet = () => {
         })
     }
     return (
-        <div className='container' style={{maxWidth: "600px", margin: "0 auto"}}>
+        <div className='container' style={{ maxWidth: "600px", margin: "0 auto" }}>
             {
                 isLoading ?
                     <Spinner /> :
                     <>
+                        <div className='flex justify-between text-center'>
+                            <h1 className='text-3xl my-4'>Write a tweet</h1>
+                        </div>
                         <div className='my-4'>
-                            <textarea type='text'placeholder='tweet...' value={tweet} onChange={(e) => setTweet(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            <textarea type='text' placeholder='tweet...' value={tweet} onChange={(e) => setTweet(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <button className="btn primary-btn me-3" onClick={() => handelAddTweet()}>
                             Save
