514f044ce7c7dca0ecca92275bf0101778417f34
karthickelango <karthik5ive@gmail.com>
2024-02-09 12:54:27 +0530

    add spacing

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -85,7 +85,7 @@ const Account = () => {
         isLoading ? <Spinner /> :
           <div className='container' style={{ maxWidth: "600px", margin: "0 auto" }}>
             <ul role="list" className="divide-y divide-gray-100 profile-page">
-              <li className="flex justify-between gap-x-6 py-5">
+              <li className="flex justify-between gap-x-6 pt-5 pb-2">
                 <div className="flex min-w-0 gap-x-4">
                   <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                   <div className="min-w-0 flex-auto">
